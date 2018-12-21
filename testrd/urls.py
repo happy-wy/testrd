f4771493c7226f2d306cd43da6751da7ec07cbfe
happy-wy <815159972@qq.com>
2018-12-21 15:00:30 +0800

    for sign

--- testrd/urls.py
+++ testrd/urls.py
@@ -25,4 +25,9 @@ urlpatterns = [
     path('login_action/', views.login_action),
     path('event_manage/', views.event_manage),
     url(r'^accounts/login/$', views.index),
+    url(r'^search_name/$', views.search_name),
+    url(r'^guest_manage/', views.guest_manage),
+    url(r'^search_realname/$', views.search_realname),
+    url(r'^sign_index/(?P<eid>[0-9]+)/$', views.sign_index),
+    url(r'^sign_index_action/(?P<eid>[0-9]+)/$', views.sign_index_action),
 ]
